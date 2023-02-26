0eb8c0c28055d1707c86e5541f952a3da694a31d
Joshua de Reeper <joshua@dereeper.co.nz>
2023-02-26 18:15:51 +1300

    DolphiniOS: Add Simple Skylander Portal

--- Source/Core/Core/IOS/USB/Emulated/Skylander.cpp
+++ Source/Core/Core/IOS/USB/Emulated/Skylander.cpp
@@ -1234,6 +1234,11 @@ bool SkylanderPortal::CreateSkylander(const std::string& file_path, u16 sky_id, 
   return true;
 }
 
+bool SkylanderPortal::RemoveSkylanderiOS(u8 sky_num)
+{
+  return Core::System::GetInstance().GetSkylanderPortal().RemoveSkylander(sky_num);
+}
+
 bool SkylanderPortal::RemoveSkylander(u8 sky_num)
 {
   if (!IsSkylanderNumberValid(sky_num))
@@ -1258,6 +1263,11 @@ bool SkylanderPortal::RemoveSkylander(u8 sky_num)
   }
 
   return false;
+}
+
+u8 SkylanderPortal::LoadSkylanderiOS(u8* buf, File::IOFile in_file)
+{
+  return Core::System::GetInstance().GetSkylanderPortal().LoadSkylander(buf, std::move(in_file));
 }
 
 u8 SkylanderPortal::LoadSkylander(u8* buf, File::IOFile in_file)
